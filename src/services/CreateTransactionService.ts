7fe16dae2a656ac65e6d9e1293c74d18c18dd786
Daniel Backes <danielbackes@protonmail.com>
2020-07-29 21:34:14 -0400

    Create transactions route.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,51 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
 
+// import AppError from '../errors/AppError';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryTitle: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+
+    let category = await categoriesRepository.findOne({
+      where: {
+        title: categoryTitle,
+      },
+    });
+
+    if (!category) {
+      category = await categoriesRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoriesRepository.save(category);
+    }
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
